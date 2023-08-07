a813c36bee81af41693467e6005d20ec2b75ad34
Sarah Schultz <131220620+sarahschultz@users.noreply.github.com>
2023-08-07 10:07:58 -0500

    created client lawyer and case classes

--- 6LINQ.cs
+++ 6LINQ.cs
@@ -1,1 +1,22 @@
 // LINQ Cheatsheet
+
+namespace LinqCheatsheet
+
+public class Lawyer
+{
+    public List<Case> Cases { get; set;} = default!;
+
+    public string FirstName {get; set;} = default!;
+
+    public string LastName { get; set; } = default!;
+
+}
+public class Client
+{
+    public List<Case> Cases { get; set;} = default!;
+
+    public string FirstName {get; set;} = default!;
+
+    public string LastName { get; set; } = default!;
+
+}
---
+++ Case.cs
@@ -0,0 +1,9 @@
+namespace LinqCheatsheet
+
+public class Case
+{
+    public Client Client { get; set; }  = default!;
+    public Lawyer Lawyer { get; set; } = default!;  
+
+    public public string MyProperty { get; set; }
+}
